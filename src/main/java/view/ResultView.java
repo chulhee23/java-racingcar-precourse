9b82be20949a96cf4c549818b6ba359d8e5ee1d6
chulhee23 <chulhee2351@gmail.com>
2021-10-19 15:31:25 +0900

    refactor(ResultView): refactoring -calculating winners

--- src/main/java/view/ResultView.java
+++ src/main/java/view/ResultView.java
@@ -6,18 +6,31 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ResultView {
 
     public static void showRoundResult(List<Car> cars) {
+
         for (Car car : cars) {
             System.out.println(car.getName() + " : " +  "-".repeat(car.getPosition()));
         }
+
     }
 
     public static void showWinner(List<Car> cars){
-        List<Car> winners = new ArrayList<>();
 
+        List<Car> winners = calculateWinners(cars);
+
+        String winnerNames = winners.stream().map(car -> car.getName()).collect(Collectors.joining(", "));
+        System.out.println("Winners = " + winnerNames);
+
+
+    }
+
+    private static List<Car> calculateWinners(List<Car> cars){
+
+        List<Car> winners = new ArrayList<>();
         int longestDistance = cars.stream().max(Comparator.comparing(car -> car.getPosition())).get().getPosition();
 
         for (Car car : cars) {
@@ -26,11 +39,6 @@ public class ResultView {
             }
         }
 
-        System.out.print("Winner = ");
-        for (Car winner : winners) {
-            System.out.print(winner + ", ");
-        }
-        System.out.println("longestDistance = " + longestDistance);
+        return winners;
     }
-
 }
